8f3ae880ec89a5d6235d3e635aecb87272e2563d
FranzCSUF <fsalvador79@gmail.com>
2018-07-23 12:50:09 -0700

    Create a constructor function in the flashcard component and add the state of the flashcard application in order to save the user's input. This will be in the form of an array of objects. Initial state will be an empty array. The state will contain topic, question, and answer for each flashcard.#1

--- flashcard.js
+++ flashcard.js
@@ -2,6 +2,10 @@ import React from 'react'
 import FlashcardBuild from './flashcardbuild'
 
 export default class Flashcard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = []
+  }
   render() {
     return (
       <FlashcardBuild/>
