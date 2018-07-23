02617db2a40e57d34e124cae42e2b24fd33b52e8
FranzCSUF <fsalvador79@gmail.com>
2018-07-23 14:55:53 -0700

    Add an event listener to the Save button that saves the user input to the state of the flash card component.#1

--- flashcard.js
+++ flashcard.js
@@ -4,11 +4,26 @@ import FlashcardBuild from './flashcardbuild'
 export default class Flashcard extends React.Component {
   constructor(props) {
     super(props)
-    this.state = []
+    this.state = {
+      flashcard: []
+    }
+    this.saveOnClick = this.saveOnClick.bind(this)
+  }
+
+  saveOnClick() {
+    const question = document.getElementById('question').value
+    const answer = document.getElementById('answer').value
+    const newCard = {question, answer}
+    const flashCardStateCopy = this.state.flashcard.slice(0)
+    flashCardStateCopy.push(newCard)
+    this.setState({flashcard: flashCardStateCopy})
+    console.log(this.state)
+    document.getElementById('question').value = ''
+    document.getElementById('answer').value = ''
   }
   render() {
     return (
-      <FlashcardBuild/>
+      <FlashcardBuild saveOnClick={this.saveOnClick}/>
     )
   }
 }
