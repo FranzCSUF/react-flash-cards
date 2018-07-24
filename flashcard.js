3bf7e857f8bd1c7a2ad359ca0d0264ba426de3e8
FranzCSUF <fsalvador79@gmail.com>
2018-07-24 10:08:08 -0700

    Add an onClick event listener to the Cards tab that updates the current view to Cards.#2

--- flashcard.js
+++ flashcard.js
@@ -6,9 +6,11 @@ export default class Flashcard extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      flashcard: []
+      view: 'New',
+      flashcards: []
     }
     this.saveOnClick = this.saveOnClick.bind(this)
+    this.cardsOnClick = this.cardsOnClick.bind(this)
   }
 
   saveOnClick(event) {
@@ -19,16 +21,22 @@ export default class Flashcard extends React.Component {
     for (var pair of formData.entries()) {
       cardObj[pair[0]] = pair[1]
     }
-    const flashCardStateCopy = this.state.flashcard.slice(0)
+    const flashCardStateCopy = this.state.flashcards.slice(0)
     flashCardStateCopy.push(cardObj)
-    this.setState({flashcard: flashCardStateCopy})
+    this.setState({flashcards: flashCardStateCopy})
     cardForm.reset()
+    console.log(this.state)
+  }
+
+  cardsOnClick (event) {
+    this.setState({view: 'Cards'})
+    console.log(this.state)
   }
 
   render() {
     return (
       <div>
-        <Navigation/>
+        <Navigation cardsOnClick={this.cardsOnClick}/>
         <FlashcardBuild saveOnClick={this.saveOnClick}/>
       </div>
     )
--- navbar.js
+++ navbar.js
@@ -2,14 +2,18 @@ import React from 'react'
 
 export default function Navigation(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">Cram Cards</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Cards</a>
+            <a className="nav-link" href="#" onClick={props.cardsOnClick}>Cards</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">New</a>
+            <a className="nav-link" href="#">Create New</a>
           </li>
         </ul>
       </div>
