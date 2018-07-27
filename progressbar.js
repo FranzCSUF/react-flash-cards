7f4bd596e568ea93afde16edc1930cfcc528b240
FranzCSUF <fsalvador79@gmail.com>
2018-07-26 21:13:34 -0700

    The progress bar should reflect the percentage of how many cards the user has completed.#7

--- progressbar.js
+++ progressbar.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function ProgressBar(props) {
   return(
-    <div className="progress">
-      <div className="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+    <div>
+      <div className="progress">
+        <div className="progress-bar" id="progress-bar" role="progressbar" style={{ width: props.progress + '%' }}></div>
+      </div>
+      <div id="message-completed"></div>
     </div>
   )
 }
