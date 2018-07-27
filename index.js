9f41ae96cd4ae7299074780057364a36f9a0d595
FranzCSUF <fsalvador79@gmail.com>
2018-07-27 15:27:53 -0700

    Rename root component to something like <App/> or <FlashCardApp/>.#15

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Flashcard from './flash-card'
+import FlashCardApp from './flash-card'
 
 ReactDOM.render(
-  <Flashcard/>,
+  <FlashCardApp/>,
   document.getElementById('app')
 )
