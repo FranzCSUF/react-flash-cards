f2a66ab4e51b0e20eccfc62d5fe850712dd77399
FranzCSUF <fsalvador79@gmail.com>
2018-08-02 14:25:00 -0700

    Add failed and correct icons to practice card controls.#24

--- practice-controls.js
+++ practice-controls.js
@@ -4,7 +4,9 @@ export default function Controls(props) {
   return (
   <div className="practice-controls">
     <i className="fas fa-angle-left prev-slider" onClick={props.handlePrev}></i>
+    <i className="fas fa-times-circle failed-icon"></i>
     <i className="fas fa-angle-right next-slider" onClick={props.handleNext}></i>
+    <i className="fas fa-check-circle correct-icon"></i>
   </div>
   )
 }
